6e32ab054d07419cf876110e0c3bce5c0da40ec7
Banyc <36535895+Banyc@users.noreply.github.com>
2023-12-02 23:11:38 +0800

    feat: alias

--- src/sql_parser.rs
+++ src/sql_parser.rs
@@ -12,6 +12,15 @@ pub enum Stat {
     Filter(FilterStat),
 }
 
+// fn parser<'a>() -> impl Parser<'a, &'a [Token], S, extra::Err<Rich<'a, Token>>> {
+//     let select = todo!();
+//     let group_agg = todo!();
+//     let filter = todo!();
+//     let stat = choice((select, group_agg, filter));
+
+//     stat.repeated().collect()
+// }
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct SelectStat {
     pub columns: Vec<Expr>,
@@ -36,6 +45,18 @@ pub enum Expr {
     Unary(Box<UnaryExpr>),
     Agg(Box<AggExpr>),
     Alias(Box<AliasExpr>),
+}
+
+fn expr<'a>() -> impl Parser<'a, &'a [Token], Expr, extra::Err<Rich<'a, Token>>> + Clone {
+    recursive(|expr| {
+        let parens = expr
+            .clone()
+            .nested_in(select_ref! { Token::Parens(parens) => parens.as_slice() });
+        let literal = select_ref! { Token::Literal(lit) => lit.clone() }.map(Expr::Literal);
+        let atom = choice((literal, parens));
+        let alias = alias_expr(expr.clone()).map(Box::new).map(Expr::Alias);
+        choice((atom, alias))
+    })
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -84,8 +105,14 @@ pub struct AliasExpr {
     pub expr: Expr,
 }
 
-fn parser<'a>() -> impl Parser<'a, &'a Token, S, extra::Err<Rich<'a, Token>>> {
-    todo!()
+fn alias_expr<'a>(
+    expr: impl Parser<'a, &'a [Token], Expr, extra::Err<Rich<'a, Token>>> + Clone,
+) -> impl Parser<'a, &'a [Token], AliasExpr, extra::Err<Rich<'a, Token>>> + Clone {
+    let alias = just(Token::Alias);
+    let name = select_ref! { Token::Literal(Literal::String(name)) => name.clone() };
+    alias
+        .ignore_then(name.then(expr))
+        .map(|(name, expr)| AliasExpr { name, expr })
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -93,12 +120,11 @@ enum Token {
     Select,
     GroupBy,
     Agg,
-    Filter,
     AggOperator(AggOperator),
-    LeftBracket,
-    RightBracket,
-    LeftParenthesis,
-    RightParenthesis,
+    Filter,
+    Alias,
+    Parens(Vec<Token>),
+    Brackets(Vec<Token>),
     LeftAngle,
     RightAngle,
     Add,
@@ -118,63 +144,69 @@ pub enum Literal {
     Number(String),
 }
 
-fn lexer<'a>() -> impl Parser<'a, &'a str, Vec<Token>, extra::Err<Rich<'a, char>>> {
-    let select = text::keyword("select").to(Token::Select);
-    let group_by = text::keyword("group").to(Token::GroupBy);
-    let agg = text::keyword("agg").to(Token::Agg);
-    let sum = text::keyword("sum").to(AggOperator::Sum);
-    let count = text::keyword("count").to(AggOperator::Count);
-    let agg_op = choice((sum, count)).map(Token::AggOperator);
-    let filter = text::keyword("filter").to(Token::Filter);
-    let left_bracket = just('[').to(Token::LeftBracket);
-    let right_bracket = just(']').to(Token::RightBracket);
-    let left_parenthesis = just('(').to(Token::LeftParenthesis);
-    let right_parenthesis = just(')').to(Token::RightParenthesis);
-    let left_angle = just('<').to(Token::LeftAngle);
-    let right_angle = just('>').to(Token::RightAngle);
-    let add = just('+').to(Token::Add);
-    let sub = just('-').to(Token::Sub);
-    let mul = just('*').to(Token::Mul);
-    let div = just('/').to(Token::Div);
-    let eq = just('=').to(Token::Eq);
-    let bang = just('!').to(Token::Bang);
-    let comma = just(',').to(Token::Comma);
-    let ident = text::ident().map(ToString::to_string).map(Token::Variable);
-    let pos_number = text::digits(10)
-        .then(just('.').then(text::digits(10)).or_not())
-        .to_slice()
-        .map(ToString::to_string)
-        .map(Literal::Number)
-        .map(Token::Literal);
-    let string = string().map(Literal::String).map(Token::Literal);
-    let token = choice((
-        select,
-        group_by,
-        agg,
-        agg_op,
-        filter,
-        left_bracket,
-        right_bracket,
-        left_parenthesis,
-        right_parenthesis,
-        left_angle,
-        right_angle,
-        add,
-        sub,
-        mul,
-        div,
-        eq,
-        bang,
-        comma,
-        ident,
-        pos_number,
-        string,
-    ));
-    token.padded().repeated().collect()
+fn lexer<'a>() -> impl Parser<'a, &'a str, Vec<Token>, extra::Err<Rich<'a, char>>> + Clone {
+    recursive(|tokens| {
+        let select = text::keyword("select").to(Token::Select);
+        let group_by = text::keyword("group").to(Token::GroupBy);
+        let agg = text::keyword("agg").to(Token::Agg);
+        let sum = text::keyword("sum").to(AggOperator::Sum);
+        let count = text::keyword("count").to(AggOperator::Count);
+        let agg_op = choice((sum, count)).map(Token::AggOperator);
+        let filter = text::keyword("filter").to(Token::Filter);
+        let alias = text::keyword("alias").to(Token::Alias);
+        let parens = tokens
+            .clone()
+            .delimited_by(just('(').padded(), just(')').padded())
+            .map(Token::Parens);
+        let brackets = tokens
+            .clone()
+            .delimited_by(just('[').padded(), just(']').padded())
+            .map(Token::Brackets);
+        let left_angle = just('<').to(Token::LeftAngle);
+        let right_angle = just('>').to(Token::RightAngle);
+        let add = just('+').to(Token::Add);
+        let sub = just('-').to(Token::Sub);
+        let mul = just('*').to(Token::Mul);
+        let div = just('/').to(Token::Div);
+        let eq = just('=').to(Token::Eq);
+        let bang = just('!').to(Token::Bang);
+        let comma = just(',').to(Token::Comma);
+        let ident = text::ident().map(ToString::to_string).map(Token::Variable);
+        let pos_number = text::digits(10)
+            .then(just('.').then(text::digits(10)).or_not())
+            .to_slice()
+            .map(ToString::to_string)
+            .map(Literal::Number)
+            .map(Token::Literal);
+        let string = string().map(Literal::String).map(Token::Literal);
+        let token = choice((
+            select,
+            group_by,
+            agg,
+            agg_op,
+            filter,
+            alias,
+            brackets,
+            parens,
+            left_angle,
+            right_angle,
+            add,
+            sub,
+            mul,
+            div,
+            eq,
+            bang,
+            comma,
+            ident,
+            pos_number,
+            string,
+        ));
+        token.padded().repeated().collect()
+    })
 }
 
 /// Ref: <https://github.com/zesterer/chumsky/blob/dce5918bd2dad591ab399d2e191254640a9ed14f/examples/json.rs#L64>
-fn string<'a>() -> impl Parser<'a, &'a str, String, extra::Err<Rich<'a, char>>> {
+fn string<'a>() -> impl Parser<'a, &'a str, String, extra::Err<Rich<'a, char>>> + Clone {
     let escape = just('\\')
         .then(choice((
             just('\\'),
@@ -212,8 +244,33 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_alias_expr() {
+        let src = r#"(alias "foo" 42)"#;
+        let lexer = lexer();
+        let tokens = lexer.parse(src).unwrap();
+        let parser = expr();
+        let expr = parser.parse(&tokens).unwrap();
+        assert_eq!(
+            expr,
+            Expr::Alias(Box::new(AliasExpr {
+                name: String::from("foo"),
+                expr: Expr::Literal(Literal::Number(String::from("42")))
+            }))
+        );
+    }
+
+    #[test]
+    fn test_lexer_empty() {
+        let src = "";
+        let lexer = lexer();
+        let tokens = lexer.parse(src).unwrap();
+        assert!(tokens.is_empty());
+    }
+
+    #[test]
     fn test_lexer() {
-        let src = r#"select group agg sum count filter [ ] ( ) < > hi + - * / = -42 0.1 "hi""#;
+        let src =
+            r#"select group agg sum count filter alias [ ] ( ) < > hi + - * / = -42 0.1 "hi""#;
         let lexer = lexer();
         let tokens = lexer.parse(src).unwrap();
         assert_eq!(
@@ -225,10 +282,9 @@ mod tests {
                 Token::AggOperator(AggOperator::Sum),
                 Token::AggOperator(AggOperator::Count),
                 Token::Filter,
-                Token::LeftBracket,
-                Token::RightBracket,
-                Token::LeftParenthesis,
-                Token::RightParenthesis,
+                Token::Alias,
+                Token::Brackets(vec![]),
+                Token::Parens(vec![]),
                 Token::LeftAngle,
                 Token::RightAngle,
                 Token::Variable(String::from("hi")),
