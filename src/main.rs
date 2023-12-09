f63c128e0dcf34d912d5f4c08aac94ea0a2cc213
Banyc <36535895+Banyc@users.noreply.github.com>
2023-12-09 16:30:55 +0800

    feat: json, jsonl, ndjson files

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,11 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use anyhow::Context;
+use anyhow::{bail, Context};
 use clap::Parser;
 use fancy_regex::Regex;
 use handler::{HandleLineResult, LineExecutor};
-use polars::{
-    error::PolarsError,
-    frame::DataFrame,
-    io::{csv::CsvWriter, SerWriter},
-    lazy::frame::{LazyCsvReader, LazyFileListReader, LazyFrame},
-};
+use polars::prelude::*;
 use rustyline::{
     error::ReadlineError, highlight::Highlighter, history::History, Completer, Editor, Helper,
     Hinter, Validator,
@@ -162,14 +157,31 @@ impl Cli {
     }
 }
 
-fn read_df_file(
-    path: impl AsRef<Path>,
-    infer_schema_length: usize,
-) -> Result<LazyFrame, PolarsError> {
-    LazyCsvReader::new(&path)
-        .has_header(true)
-        .with_infer_schema_length(Some(infer_schema_length))
-        .finish()
+fn read_df_file(path: impl AsRef<Path>, infer_schema_length: usize) -> anyhow::Result<LazyFrame> {
+    let Some(extension) = path.as_ref().extension() else {
+        bail!(
+            "No extension at the name of the file `{}`",
+            path.as_ref().to_string_lossy()
+        );
+    };
+    Ok(match extension.to_string_lossy().as_ref() {
+        "csv" => LazyCsvReader::new(&path)
+            .has_header(true)
+            .with_infer_schema_length(Some(infer_schema_length))
+            .finish()?,
+        "json" => {
+            let file = std::fs::File::options().read(true).open(&path)?;
+            JsonReader::new(file).finish()?.lazy()
+        }
+        "ndjson" | "jsonl" => LazyJsonLineReader::new(&path)
+            .with_infer_schema_length(Some(infer_schema_length))
+            .finish()?,
+        _ => bail!(
+            "Unknown extension `{}` at the name of the file `{}`",
+            extension.to_string_lossy(),
+            path.as_ref().to_string_lossy()
+        ),
+    })
 }
 
 pub mod handler {
@@ -257,12 +269,32 @@ pub mod handler {
 }
 
 fn write_df_output(mut df: DataFrame, path: impl AsRef<Path>) -> anyhow::Result<()> {
+    let Some(extension) = path.as_ref().extension() else {
+        bail!(
+            "No extension at the name of the file `{}`",
+            path.as_ref().to_string_lossy()
+        );
+    };
     let _ = std::fs::remove_file(&path);
     let output = std::fs::File::options()
         .write(true)
         .create(true)
-        .open(path)?;
-    CsvWriter::new(output).finish(&mut df)?;
+        .open(&path)?;
+    match extension.to_string_lossy().as_ref() {
+        "csv" => CsvWriter::new(output).finish(&mut df)?,
+        "json" => JsonWriter::new(output).finish(&mut df)?,
+        "ndjson" | "jsonl" => {
+            bail!(
+                "No `JsonLineWriter` available to write `{}`",
+                path.as_ref().to_string_lossy()
+            );
+        }
+        _ => bail!(
+            "Unknown extension `{}` at the name of the file `{}`",
+            extension.to_string_lossy(),
+            path.as_ref().to_string_lossy()
+        ),
+    }
     Ok(())
 }
 
